68cb31642441e68e49993b4f10ef59b08766b697
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-10-20 17:39:25 -0400

    Added ejs index rooms pages

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var app = express()
+const expressLayouts = require('express-ejs-layouts')
 
 app.use(express.static('public'))
 app.use(express.static('node_modules/bootstrap/dist'))
 
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
+app.set('view engine', 'ejs')
+
+app.get('/rooms', (req, res) => {
+    res.render('rooms')
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index')
 });
 
 app.listen(3000, () => {
